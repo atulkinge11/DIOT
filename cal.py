b85f79139b20eede0dc867ebf0c9b1549cdad404
atulkinge11 <atulkinge11@gmail.com>
2023-01-19 02:24:40 +0530

    i am diot

---
+++ cal.py
@@ -0,0 +1,14 @@
+def add(num1,num2):
+    return num1+num2
+
+def sub(num1,num2):
+    return num1-num2
+
+res_add = add(10,20)
+print(res_add)
+
+res_sub = sub(50,20)
+print(res_sub)
+
+
+#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
